a654437f259db13d1676a617f0b7bc23e278137e
sanjay <sskachchhava@gmail.com>
2021-11-20 23:16:49 +0530

    final commit

---
+++ WDDN_DotNet_Project_CE029_CE060_CE062/WebApiASPDotNetCore/Models/DatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoWebApi.Models
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options):base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<User> Users { get;set; }
+        public DbSet<UserModelTemp> TempUsers { get; set; }
+        public DbSet<VarificationModel> VarificationModel { get; set; }
+
+
+    }
+}
---
+++ WDDN_DotNet_Project_CE029_CE060_CE062/WebApiASPDotNetCore/Models/VarificationModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoWebApi.Models
+{
+    public class VarificationModel
+    {
+        private EntityEntry<UserModelTemp> tempUser;
+        private int code;
+        private DateTime now;
+
+        public VarificationModel() { }
+        public VarificationModel(UserModelTemp user, int code, DateTime time)
+        {
+            this.User = user;
+            this.VarificationCode = code;
+            this.CreateDate = time;
+        }
+
+        public VarificationModel(EntityEntry<UserModelTemp> tempUser, int code, DateTime now)
+        {
+            this.tempUser = tempUser;
+            this.code = code;
+            this.now = now;
+        }
+
+        public int Id { get; set; }
+        public UserModelTemp User { get; set; }
+        public int VarificationCode { get; set; }
+        public DateTime CreateDate { get; set; }
+
+    }
+}
